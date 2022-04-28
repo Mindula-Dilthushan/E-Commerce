a0b9540f41d1bb727946fd66205740a25a45cf2f
Mindula Dilthushan Manamperi <minduladilthushan1@gmail.com>
2022-04-28 13:59:26 +0530

    backend-init

---
+++ E-Commerce-Backend/src/main/java/com/example/ecommercebackend/ECommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommercebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceBackendApplication.class, args);
+    }
+
+}
---
+++ E-Commerce-Backend/src/test/java/com/example/ecommercebackend/ECommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommercebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
